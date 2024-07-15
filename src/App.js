29dffc1a3b8dcf34db1b481bab2aac73519b8eb7
Clentan <clentanchauke9089@gmail.com>
2024-07-15 23:52:18 +0200

    nhnh

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+
+    <><div className="App">
+      <div className='one'></div>
+      <div className='two'></div>
+      <div className='three'></div>
+    </div><div className='App2'>
+        <div className='four'></div>
+        <div className='five'></div>
+        <div className='six'></div>
+      </div><div className='App3'>
+        <div className='eight'></div>
+        <div className='nine'></div>
+        <div className='ten'></div>
+      </div></>
+  );
+}
+
+export default App;
